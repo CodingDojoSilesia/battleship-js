d7f6eac263e6158dd3b5f7a6c063c717c650a232
Fajkowsky <dawid.fajkowski@gmail.com>
2019-08-16 18:45:32 +0200

    Initial setup with webpack & babel

---
+++ README.md
@@ -0,0 +1,12 @@
+# Battleships
+
+## How to run
+1. Install [Node.js](https://nodejs.org/en/download/)
+2. Install [yarn](https://yarnpkg.com/en/docs/install)
+3. `yarn install`
+4. `yarn run dev`
+5. Run tests with: `yarn run test`
+## Useful links
+1. https://webpack.js.org
+2. https://babeljs.io
+3. https://jasmine.github.io
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: ['@babel/polyfill', './src/app.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules : [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    })
+  ],
+};
+
+module.exports = config;
